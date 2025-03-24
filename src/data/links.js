a67031d716f8fe5e6cc718fc1d201cb12ad9695b
Fino <brescatomas@gmail.com>
2025-03-24 18:14:11 -0300

    Estrucutra en distintas páginas

---
+++ src/data/links.js
@@ -0,0 +1,12 @@
+// src/data/links.js
+export const pageLinks = {
+  "01": "https://wa.me/5492233464125?text=Holaa%20quiero%20crear%20mi%20cuenta",
+  "02": "https://wa.me/5491131582726?text=Holaa%20quiero%20crear%20mi%20cuenta",
+  "03": "https://wa.me/5491155608678?text=Holaa%20quiero%20crear%20mi%20cuenta",
+  "04": "https://wa.me/5493442684863?text=Holaa%20quiero%20crear%20mi%20cuenta",
+  "05": "https://wa.me/5491126737466?text=Holaa%20quiero%20crear%20mi%20cuenta",
+  "06": "https://wa.me/5491130994384?text=Holaa%20quiero%20crear%20mi%20cuenta",
+  "07": "https://wa.me/5492216921136?text=Holaa%20quiero%20crear%20mi%20cuenta",
+  "08": "https://wa.me/5491155837639?text=Holaa%20quiero%20crear%20mi%20cuenta",
+  "09": "https://wa.me/5492252449304?text=Holaa%20quiero%20crear%20mi%20cuenta"
+};
